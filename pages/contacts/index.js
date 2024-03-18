3918f8b423209e2b9e3e3624fafff59027aeee60
tema-skakun <frederico_f@mail.ru>
2024-03-18 17:36:38 +0100

    feat: adds fetching data

--- pages/contacts/index.js
+++ pages/contacts/index.js
@@ -1,12 +1,32 @@
 import Head from "next/head";
 import Heading from "@/components/Heading";
 
-const Contacts = () => (
+export const getStaticProps = async () => {
+  const response = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await response.json();
+
+  if (!data) {
+    return {
+      notFound: true,
+    }
+  }
+
+  return {
+    props: { contacts: data },
+  }
+}
+
+const Contacts = ({ contacts }) => (
   <>
     <Head>
       <title>Contacts</title>
     </Head>
     <Heading text="Contacts list:"/>
+    <ul>
+      {contacts && contacts.map(({ id, name, email }) => (
+        <li key={id}><strong>{name}</strong> ({email})</li>
+      ))}
+    </ul>
   </>
 );
 
